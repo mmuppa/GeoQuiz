3c42688dc98b176abbed172a40b85f5164d18c0b
brianadennis <brianadennis@hotmail.com>
2015-06-30 10:08:00 -0700

    test

--- app/src/main/java/edu/uw/tacoma/mmuppa/geoquiz/MainActivity.java
+++ app/src/main/java/edu/uw/tacoma/mmuppa/geoquiz/MainActivity.java
@@ -17,7 +17,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
+        log.d("yay");
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
